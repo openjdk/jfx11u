004ef52d4ffb7ba3c8a9ee323c78f1340dc7037b
Alexander Matveev <almatvee@openjdk.org>
2022-11-02 20:21:17 +0000

    8294400: Provide media support for libavcodec version 59
    Reviewed-by: kcr, jvos

--- modules/javafx.media/src/main/java/com/sun/media/jfxmediaimpl/NativeMediaManager.java
+++ modules/javafx.media/src/main/java/com/sun/media/jfxmediaimpl/NativeMediaManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -124,6 +124,7 @@ public class NativeMediaManager {
                     dependencies.add("avplugin-ffmpeg-56");
                     dependencies.add("avplugin-ffmpeg-57");
                     dependencies.add("avplugin-ffmpeg-58");
+                    dependencies.add("avplugin-ffmpeg-59");
                 }
                 if (HostUtils.isMacOSX()) {
                     dependencies.add("fxplugins");
